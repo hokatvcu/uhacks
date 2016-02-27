88a9ef61258fe354e681f8fd07fcb0bc936da06f
Kenny Ho <hok@vcu.edu>
2016-02-27 07:28:54 -0500

    change settings

--- uhacks/settings.py
+++ uhacks/settings.py
@@ -37,7 +37,7 @@ ENV_ROLE = get_env_variable('ENV_ROLE')
 SECRET_KEY = get_env_variable('UHACKS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 UHACKS_DB_PASS = False
 if ENV_ROLE == 'development':
